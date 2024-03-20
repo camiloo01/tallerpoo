826daad77201e55fa4fed566747b5a7486fdd1b3
Your Name <you@example.com>
2024-03-19 20:19:54 -0500

    hola

---
+++ Punto1/Punto1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Cuenta
+{
+    public int saldo; // Ahora el saldo no es privado
+
+    // Constructor que valida el saldo inicial
+    public Cuenta(int saldoInicial)
+    {
+        if (saldoInicial >= 0)
+        {
+            saldo = saldoInicial;
+        }
+        else
+        {
+            saldo = 0;
+            Console.WriteLine("Error: El saldo inicial era invalido, see establecio el saldo en 0");
+        }
+    }
+
+    // añadir saldo
+    public void Credit(int monto)
+    {
+        saldo += monto;
+        Console.WriteLine($"Se añadio {monto} a la cuenta, saldo actual: {saldo}");
+    }
+
+    // retirar saldo de la cuenta
+    public void Cargar(int monto)
+    {
+        if (monto <= saldo)
+        {
+            saldo -= monto;
+            Console.WriteLine($"Se retiro {monto} de la cuenta, saldo actual: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("El monto excede el saldo de la cuenta");
+        }
+    }
+
+    // Función para el saldo actual
+    public int ObtenerSaldo()
+    {
+        return saldo;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear objetos cuenta
+        Cuenta cuenta1 = new Cuenta(100); // Saldo inicial valido
+        Cuenta cuenta2 = new Cuenta(-50); // Saldo inicial invalido
+
+        // funciones miembros de la clase
+        cuenta1.Credit(50);
+        cuenta1.Cargar(30);
+        Console.WriteLine("Saldo de cuenta 1: " + cuenta1.ObtenerSaldo());
+
+        cuenta2.Credit(20);
+        cuenta2.Cargar(100);
+        Console.WriteLine("Saldo de cuenta 2: " + cuenta2.ObtenerSaldo());
+        Console.ReadLine();
+    }
+}
+
---
+++ Punto2/Punto2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Factura
+{
+    // Atributos publicos
+    public string NumeroPieza;
+    public string DescripcionPieza;
+    public int Cantidad;
+    public int PrecioPorArticulo;
+
+    // Constructor
+    public Factura(string numeroPieza, string descripcionPieza, int cantidad, int precioPorArticulo)
+    {
+        NumeroPieza = numeroPieza;
+        DescripcionPieza = descripcionPieza;
+        Cantidad = cantidad;
+        PrecioPorArticulo = precioPorArticulo;
+    }
+
+    // Funcion para obtener
+    public int ObtenerMontoFactura()
+    {
+        return Cantidad * PrecioPorArticulo;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear objeto Factura y probar sus funciones
+        Factura factura1 = new Factura("001", "Martillo", 5, 100); // Cantidad y precio valido
+        Factura factura2 = new Factura("002", "Clavos", -10, -5);
+
+        Console.WriteLine("Monto de factura 1: $" + factura1.ObtenerMontoFactura());
+        Console.WriteLine("Monto de factura 2: $" + factura2.ObtenerMontoFactura());
+        Console.ReadLine();
+    }
+}
+
